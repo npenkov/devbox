78d451458a2dd291c22782744bfeab87016d7f0e
Nick Penkov <nick.penkov@gmail.com>
2024-06-15 14:16:19 +0200

    devbox commit

--- init.sh
+++ init.sh
@@ -1,26 +1,26 @@
-shell=`ps -p $$ | awk 'NR>1  {print $4}' | sed 's/-//g'`
+shell=$(ps -p $$ | awk 'NR>1  {print $4}' | sed 's/-//g')
 SCRIPT_PATH="$HOME/.local/share/devbox/global/current"
 
 case $(basename $shell) in
-     "zsh" )
-            . $DEVBOX_GLOBAL_ROOT/zsh/.zshrc
-           ;;
-     "bash" )
-            . $DEVBOX_GLOBAL_ROOT/bash/.bashrc
-           ;;
-     * )
-           ;;
+"zsh")
+	. $DEVBOX_GLOBAL_ROOT/zsh/.zshrc
+	;;
+"bash")
+	. $DEVBOX_GLOBAL_ROOT/bash/.bashrc
+	;;
+*) ;;
 esac
 
-# bat
-# bat --plain for unformatted cat
-alias catp='bat -P'
-# replace cat with bat
-alias cat='bat'
-# zoxide
-# zoxide for smart cd
-alias cd='z'
+## bat
+## bat --plain for unformatted cat
+#alias catp='bat -P'
+## replace cat with bat
+#alias cat='bat'
+## zoxide
+## zoxide for smart cd
+#alias cd='z'
 # devbox helpers
 alias dbr='devbox run'
 alias cddevbox='cd $DEVBOX_GLOBAL_ROOT'
-
+alias update-devbox='eval "$(devbox global shellenv --recompute)"'
+alias devbox-cleanup='devbox run -- nix store gc --extra-experimental-features nix-command'
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,14 +1,37 @@
 # add devbox bits to zsh
 fpath+=($DEVBOX_GLOBAL_PREFIX/share/zsh/site-functions $DEVBOX_GLOBAL_PREFIX/share/zsh/$ZSH_VERSION/functions $DEVBOX_GLOBAL_PREFIX/share/zsh/vendor-completions)
 autoload -U compinit && compinit
+
 # initialize apps
 # starship
-eval "$(starship init zsh)"
+#eval "$(starship init zsh)"
+
 # zoxide
 eval "$(zoxide init zsh)"
+
 # direnv
 eval "$(direnv hook zsh)"
-# atuin
-if [[ $options[zle] = on ]]; then
-  eval "$(atuin init zsh )"
-fi
+# atuin - history search
+# eval "$(atuin init zsh)"
+
+# export ZSH=$DEVBOX_GLOBAL_PREFIX/share/oh-my-zsh
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+ZSH_THEME="robbyrussell"
+
+plugins=(
+	git
+	golang
+	gh
+	kube-ps1
+	kubectl
+	z
+	zsh-navigation-tools
+)
+
+source $DEVBOX_GLOBAL_PREFIX/share/oh-my-zsh/oh-my-zsh.sh
+
+source $DEVBOX_GLOBAL_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $DEVBOX_GLOBAL_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
